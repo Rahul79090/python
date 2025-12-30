1720b09c3e2b23b0fbd30ce93123d48b8444eaa4
Rahul79090 <kumarrahul032004@gmail.com>
2025-12-30 22:54:20 +0530

    f.py

---
+++ f.py
@@ -0,0 +1,14 @@
+# print("hii")
+# print("hello world")
+# print("this is my first program")
+# print("i hope you all like it")
+# 
+"""hello world 
+this is my first pythan program 
+i hope you all like it."""
+
+#print ("hello world  \n this is my first pythan program  \n i hope you all like it")
+# a="hello world"
+# print (a)
+Name ="john"
+print(Name)
