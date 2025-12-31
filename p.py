940d66a798361595478c4f093e984ad4ff59c768
Rahul79090 <kumarrahul032004@gmail.com>
2025-12-31 20:08:37 +0530

    add.py

--- p.py
+++ p.py
@@ -1,4 +1,7 @@
-age = float (input("enter the age:-"))
-print(age)
-exp1 = eval(input("enter any equation here:-"))
-print(exp1)
+# age = float (input("enter the age:-"))
+# print(age)
+# exp1 = eval(input("enter any equation here:-"))
+# print(exp1)
+name = "jhon"
+# age =23
+print (type(name))
