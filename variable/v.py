8706cadb5a329d9ef969626af46032d62a78fdd6
Rahul79090 <kumarrahul032004@gmail.com>
2026-01-06 21:26:55 +0530

    v.py

---
+++ variable/v.py
@@ -0,0 +1,25 @@
+name ="rahul kuamr"
+age = 19
+price = 25.6
+print("my name is :-", name)
+print("my age is :-",age)
+print("my price is :-",price )
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+name1 = "rkg"
+name2 = 'rkg'
+name3 = '''rkg'''
+
+print(name1)
+print(name2)
+print(name3)
+
+
+age =23
+old = False
+a = None
+print(type(old))
+print(type(a))
---
+++ variable/v2.py
@@ -0,0 +1,14 @@
+name1 = "rkg"
+name2 = 'rkg'
+name3 = '''rkg'''
+
+print(name1)
+print(name2)
+print(name3)
+
+
+age =23
+old = False
+a = None
+print(type(old))
+print(type(a))
