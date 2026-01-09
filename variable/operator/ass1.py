380a23a1de193d0572dacb4b7bd4f1c576f97cc5
Rahul79090 <kumarrahul032004@gmail.com>
2026-01-09 20:52:05 +0530

    os.py

---
+++ variable/operator/ass1.py
@@ -0,0 +1,13 @@
+num = 10
+# num = num + 10 #10 +10=> 20
+# num+=10
+
+# print("num:-", num)
+
+num**=5
+
+print("num:-", num)
+
+# num*=10
+
+# print("num:-", num)
---
+++ variable/operator/logicalo.py
@@ -0,0 +1,20 @@
+#logical operator 
+print(not True)
+print(not False)
+
+a = 40
+b = 30
+print(a>b)
+print(a<b)
+
+
+# and operator 
+#jab dono value true ho to hi ans true ho ga nahi to fakse ho jaye ga 
+val1 = True
+val2 = False
+print("ans operator:-", val1 and val2)
+
+
+#jab dono value me se ek vi value true ho to nas true ho ga nahi to false ho jaye ga 
+
+print("ans operator:-", val1 or val2)
