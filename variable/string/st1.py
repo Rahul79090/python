389016f276dec678025870a061770a8ee338bdf9
Rahul79090 <kumarrahul032004@gmail.com>
2026-01-15 19:58:05 +0530

    greater.py

---
+++ variable/string/st1.py
@@ -0,0 +1,15 @@
+str1 = "this is string. \n we are creating it in python.  "
+len1= len(str1)
+print(len1)
+str2 = '\napna village '
+len2= len(str2)
+print(len2)
+str3 = """this is a string"""
+
+" this is apna village's "
+
+# print(str1 + str2)
+final_str = str1 + str2
+print(final_str)
+len3 = len(final_str)
+print(len3)
