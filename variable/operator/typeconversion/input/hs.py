2d8fb7f3d6f1215fa3fc1ae474b072c500998d45
Rahul79090 <kumarrahul032004@gmail.com>
2026-01-19 00:07:50 +0530

    hs.py

---
+++ variable/operator/typeconversion/input/hs.py
@@ -0,0 +1,11 @@
+# str = "apna college "
+# ch = str[5]
+# print(" character number is :-", ch)
+
+# str = "apna college "
+# str[ :4] #pna
+# print (str[ :4])
+
+str = "apna college "
+str[ 1: ]
+print(str[1: ])
