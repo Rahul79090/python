081fc286ef94c8e4c015cb91d41d15a0675a8fb9
Rahul79090 <kumarrahul032004@gmail.com>
2026-01-20 20:47:49 +0530

    temp.py

--- variable/operator/typeconversion/input/slicing/sc.py
+++ variable/operator/typeconversion/input/slicing/sc.py
@@ -1,3 +1,10 @@
+# str = "apna college "
+# str[ 1: ]
+# print(str[1: ])
 str = "apna college "
 str[ 1: ]
-print(str[1: ])
+print(str[1:len (str) ])
+
+str = "apna college "
+str[ 5: ]
+print(str[5:len (str) ])
