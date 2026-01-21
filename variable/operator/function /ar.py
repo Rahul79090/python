08490c3f2d4f1a17bee56984c5d17757317c60d7
Rahul79090 <kumarrahul032004@gmail.com>
2026-01-21 21:42:13 +0530

    ar.py

---
+++ variable/operator/function /ar.py
@@ -0,0 +1,13 @@
+# Create an array (list)
+arr = [10, 20, 30, 40, 50]
+
+# Print array
+print(arr)
+
+# Access elements
+print(arr[0])   # First element
+print(arr[2])   # Third element
+
+# Change value
+arr[1] = 25
+print(arr)
