6fddb95c2a07acaa6f371d9458359b542f4ad516
Rahul79090 <kumarrahul032004@gmail.com>
2026-01-25 22:03:14 +0530

    st4.py

---
+++ variable/operator/typeconversion/input/st4.py
@@ -0,0 +1,11 @@
+A = 10
+B = 20
+
+average = (A+B)
+print(average/2 )
+
+a = int(input("enter the first input:-"))
+b = int(input("enter the second input:-"))
+
+average = a+ b
+print("average :-", average /2)
