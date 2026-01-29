2ff56610c1a3abc5489c06b8382340cf199447ce
Rahul79090 <kumarrahul032004@gmail.com>
2026-01-30 00:02:25 +0530

    lis2.py

--- variable/conditional/list/lis2.py
+++ variable/conditional/list/lis2.py
@@ -1,4 +1,13 @@
 
-student = ["rahul",95.8, 20, "delhi"]
+# student = ["rahul",95.8, 20, "delhi"]
 
-print(student)
+# print(student[0])
+
+# student[0] ="Raju"
+# print(student)
+
+# student = ["Rahul kumar ", 95.8 ,20 , "neru tol dath"]
+# print(student)
+
+marks = [85, 94, 76, 63, 48]
+print(marks[0:3])
