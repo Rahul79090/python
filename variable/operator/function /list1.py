fdf9ffd8f132e325a3473904edac0eb2cbf3ae61
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-02 09:29:39 +0530

    list1.py

---
+++ variable/operator/function /list1.py
@@ -0,0 +1,28 @@
+# list = [2 ,1 ,3 ]
+# # list.append(4)
+# # list.sort()
+# print(list.append(4))
+# print(list.sort())
+# print(list)
+# print(list.sort(reverse=True))
+# print(list)
+
+# list= ["banana", "litchi", "apple"]
+
+# print(list.sort())
+# print(list)
+# print(list.sort(reverse=True))
+# print(list)
+
+
+# list.reversed () #reverses list
+# list = ['a','b','c','d','e','f']
+# list.reverse()
+# print(list)
+
+# list.insert(idx,el) // insert element at index
+
+list = ['a','b','c','d','e','f']
+list.insert(2,"e")
+list.pop(4)
+print(list)
