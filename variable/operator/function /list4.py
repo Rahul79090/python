deb5ff89976faf0441c1ac9beeb4aa635015ed64
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-03 10:13:18 +0530

    list4.py

---
+++ variable/operator/function /list4.py
@@ -0,0 +1,6 @@
+# wap to count the number of student with the "A" gread in the following tuple.abs
+# ["c","d","a","a","b","b","a"]
+
+list = ["C", "D", "A", "A","B","B", "A" ]
+list.count()
+print(list)
