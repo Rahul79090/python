4ab59fb48070dab2ac75f50253d0612c9d771174
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-03 22:08:17 +0530

    dic1.py

---
+++ variable/operator/function /dictionary/dic1.py
@@ -0,0 +1,9 @@
+info ={
+    "key " : "value",
+    "name" : "apnacollege"
+    # # "age" : 35,
+    # "is_adult" :true,
+    # "marks" :94.4    
+}
+
+print(info)
--- variable/operator/function /list4.py
+++ variable/operator/function /list4.py
@@ -1,6 +1,6 @@
-# wap to count the number of student with the "A" gread in the following tuple.abs
-# ["c","d","a","a","b","b","a"]
+# wap to count the number of student with the "A" grade in the following list.
+# ["C","D","A","A","B","B","A"]
 
-list = ["C", "D", "A", "A","B","B", "A" ]
-list.count()
-print(list)
+grades = ["C", "D", "A", "A","B","B", "A" ]
+count_A = grades.count("A")
+print(f"Number of students with 'A' grade: {count_A}")
