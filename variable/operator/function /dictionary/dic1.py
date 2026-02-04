1c29703fb437fe6ca97f676e09c9e2b8fc08a5d6
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-04 09:21:36 +0530

    dic2.py

--- variable/operator/function /dictionary/dic1.py
+++ variable/operator/function /dictionary/dic1.py
@@ -1,9 +1,23 @@
-info ={
-    "key " : "value",
-    "name" : "apnacollege"
-    # # "age" : 35,
-    # "is_adult" :true,
-    # "marks" :94.4    
-}
+# info ={
+#     "key " : "value",
+#     "name" : "apnacollege"
+#     # # "age" : 35,
+#     # "is_adult" :true,
+#     # "marks" :94.4    
+# }
 
-print(info)
+# print(info)
+
+info = {
+    "name" : "Rahul kumar sahani",
+    "subjects" : ["python", "c", "java"],
+    "topics" : ("direct", "set"),
+
+    "age" : 35,
+    "is_adult" : True,
+    "float_key" : 94.4
+}
+print(info["name"])
+print(info["topics"])
+print(info["subjects"])
+print(info["age"])
---
+++ variable/operator/function /dictionary/dic2.py
@@ -0,0 +1,9 @@
+student = {
+    "name ":"Rahul kumar",
+    "subject":{
+        "phy":97,
+        "chem":98,
+        "math":95
+    }
+}
+print(student["subject"]["chem"])
