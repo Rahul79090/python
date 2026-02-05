b8abad09121061b6fb753a956e239bfba00173d9
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-05 23:32:18 +0530

    dic5.py

--- variable/operator/function /dictionary/dic5.py
+++ variable/operator/function /dictionary/dic5.py
@@ -6,4 +6,20 @@ student = {
         "math": 95
     }
 }
-print(student.keys())
+#print(student.keys())
+
+# print(len(list))
+# print(student["name"])
+# print(student.get("name"))
+
+print("BEFORE")
+
+# print(student["name2"])  #error
+# print(student.get("name2")) #no error ->None
+
+print("hi")
+print("my gf name is guriya ")
+print("welcome to my village ")
+print("we are learning ")
+print("coding")
+
