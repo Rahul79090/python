e42300d49f2589b3d0a3501d62ddbcd9fc7f3935
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-09 22:23:01 +0530

    con1.py

--- variable/conditional/con1.py
+++ variable/conditional/con1.py
@@ -3,4 +3,4 @@ age =int(input("enter the age:-"))
 if(age>=18) :
     print("can vote & apply for driving license")
 else :
-    print("cannot vote & connot apply for driving lincense")
+    print("cannot vote & cannot apply for driving license")
--- variable/operator/function /dictionary/col1.py
+++ variable/operator/function /dictionary/col1.py
@@ -1,4 +1,8 @@
-collection = {1 , 2, 3, 4}
+# collection = {1 , 2, 3, 4}
 
-print(collection)
-print(type(collection))
+# print(collection)
+# print(type(collection))
+
+list1 = {1 ,2 , 3, 4 , 5 }
+list1[2]= 10
+print(list1) 
