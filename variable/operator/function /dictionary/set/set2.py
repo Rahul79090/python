03cd266add73f2ea20aa344a5e01719ed12775a8
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-10 22:32:31 +0530

    set2.py

--- variable/operator/function /dictionary/set/set2.py
+++ variable/operator/function /dictionary/set/set2.py
@@ -1,3 +1,11 @@
-list2 = [1, 2, 3, 4, 5, 6]
-list2[2] = 20
-print(list2)
+# list2 = [1, 2, 3, 4, 5, 6]
+# list2[2] = 20
+# print(list2)
+
+collection = set()
+collection.add(1)
+collection.add(2)
+collection.add(2)
+
+print(type(collection))
+print(collection)
