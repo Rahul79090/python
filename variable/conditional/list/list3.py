d4275083cc3bf79a445908fe8659231925831b73
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-12 18:11:47 +0530

    con5.py

---
+++ variable/conditional/list/list3.py
@@ -0,0 +1,22 @@
+# How do you add elements to a list? (append(), insert(), extend())
+
+# lisr1 = {2 , 3, 4 , 5}
+# list1.add(1)
+# print(list1)
+
+
+# collection = set()
+# collection.add(3)
+# collection.add(5)
+# print(collection)
+
+# collection = {3, 4 ,7, 9}
+# collection.add(2)
+# print(collection)
+
+
+# How do you remove elements from a list? (remove(), pop(), clear())
+
+set = {2 , 3, 4, 5, 6}
+set.remove(3)
+print(set)
