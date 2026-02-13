531212bd5c5ba1067d618460e5eb8ab309b3c85b
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-13 09:33:38 +0530

    loop1.py

---
+++ variable/loop/loop1.py
@@ -0,0 +1,43 @@
+# count =1
+# while count <= 5 :
+#     print("hello ")
+#     count +=1
+#     print(count)
+
+# i =1
+# while i<= 100:
+#     print("hello", i)
+#     i +=1
+
+# print number from 1 to 5
+
+# i =1
+# while i <= 5:
+#     print(i)
+#     i +=1
+
+# i =5 
+# while i >= 1:
+#     print(i)
+#     i -=1
+
+# #  print number from 1 to 100
+# count = 1
+# while count <= 100:
+#     print (count)
+#     count +=1
+
+
+# # print number from 100 to 1
+
+# count =100
+# while count >= 1:
+#     print(count)
+#     count-=1
+
+#  print the multi[lication table of a number n.
+
+i = 1
+while i <=10:
+    print(i*3)
+    i +=1
