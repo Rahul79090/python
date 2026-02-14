dfb4a82ea7244a698089ae901d34d15394c9f0b9
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-14 22:05:41 +0530

    ar.py

--- variable/loop/loop2.py
+++ variable/loop/loop2.py
@@ -11,17 +11,38 @@
 # search for a number x in this tuple using loop.
 #  (1, 4, 9, 25, 36, 49, 64, 81, 100)
 
-nums = (1, 4, 9, 25, 36, 49, 64, 81, 100)
+# nums = (1, 4, 9, 25, 36, 49, 64, 81, 100)
 
 # i =0
 # while i <len(nums):
 #     print(nums[i])
     # i +=1
 
-x =36
-i =0
-while i <len(nums):
-    if(nums[i]==x):
-        print("fount at index 5")
+# nums = (1, 4, 9, 25, 36, 49, 64, 81, 100, 36)
+# x = 36
 
-        i +=1
+# i = 0
+
+# while i < len(nums):
+#     if nums[i] == x:
+#         print("found at index", i)
+#     else:
+#             print ("finding")
+#     i += 1
+
+
+# i = 1
+# while i <=5:
+#     print(i)
+#     if(i == 3):
+#         break
+#     i +=1
+
+# print("end of loop ")
+
+i =0
+while i <= 5:
+    print(i)
+    if(i  == 3):
+        continue
+    i += 1
