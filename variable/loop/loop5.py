22a1c4b9e67ca86fc90405a4076e80454db67a0f
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-18 02:07:11 +0530

    loop5.py

--- variable/loop/loop5.py
+++ variable/loop/loop5.py
@@ -1,8 +1,16 @@
-for i in range(1, 6):
-    if i == 3:
-        continue
+# for i in range(1, 6):
+#     if i == 3:
+#         continue
+#     print(i)
+# for i in range(1, 6):
+#     if i == 3:
+#         break
+#     print(i)
+
+i = 1
+while i<=10:
+    if(i%2 ==0):
+        i +=1
+        continue #skip
     print(i)
-for i in range(1, 6):
-    if i == 3:
-        break
-    print(i)
+    i +=1
