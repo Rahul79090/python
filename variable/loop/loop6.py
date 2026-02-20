d5dae82265faef19e0cf268af1dc597dad39e0eb
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-20 10:16:43 +0530

    loop6.py

--- variable/loop/loop6.py
+++ variable/loop/loop6.py
@@ -1,7 +1,29 @@
-my_set = {10 , 20, 30, 40, 50}
+# my_set = {10 , 20, 99, 40, 50}
 
-sorted_list = sorted(my_set)
+# sorted_list = sorted(my_set)
 
-mid_index = len(sorted_list) // 2
+# mid_index = len(sorted_list) // 2
 
-print(sorted_list[mid_index])
+# print(sorted_list[mid_index])
+
+# num = [10 , 30 ,22, 33, 40]
+ 
+# sorting = sorted(num)
+# print(sorting)
+
+# list ={23,15,24,12,45,67}
+# mid_index=len(list)//2
+
+# print(len(mid_index))
+
+
+# my_list = [23, 34, 43, 44 , 45, 67]
+
+# mid_index = len(my_list) // 2
+
+# print(my_list[mid_index])
+
+veggies =["potato","brijal","ladyfinger","cucumber"]
+
+for val in veggies:
+    print(val)
