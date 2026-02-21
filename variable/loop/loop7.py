40cf9912f0083caf3eb0c5e201e2d28fe509e0ec
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-21 20:30:29 +0530

    loop7.py

--- variable/loop/loop7.py
+++ variable/loop/loop7.py
@@ -1,1 +1,52 @@
-tup = {1, 2, 3, 4,2,8}
+# tup = {1, 2, 3, 4,2,8,9}
+
+# for num in tup:
+#     print(num)
+
+# str = "rahul kumar sahani"
+
+# for char in str:
+#     print(char)
+# else:
+#     print("end")
+
+# str = "rahul kumar sahani"
+
+# for char in str:
+#     if(char =='k'):
+#         print("k is found")
+#         break
+#     print(char)
+# else:
+#     print("end")
+
+# print the element of the following list using a loop
+# [1 , 4, 9, 16, 25, 49, 64, 81, 100]
+# list = [1 , 4, 9, 16, 25, 49, 64, 81, 100]
+
+# for num in list:
+#     print(num)
+
+
+# list = (1 , 4, 9, 16, 25, 49, 64, 81, 100,49)
+
+# x =49
+# ind = 0
+# for num in list:
+#     if(num == x):
+#         print("element is found ",ind)
+#         ind +=1
+#         break
+#     print(num)
+# else:
+#     print("not found")
+
+nums = (1 , 4, 9, 16, 25, 49, 64, 81, 100,49)
+
+x =49
+
+idx = 0
+for el in nums:
+    if(el == x):
+        print("number found at idx",idx)
+    idx += 1
---
+++ variable/loop/loop8.py
@@ -0,0 +1,18 @@
+# print(range(5))
+
+# seq = range(10)
+
+# for i in seq:
+#     print(i)
+
+# for i in range(2, 10): #range(start , stop)
+
+#     print(i)
+
+
+# for i in range(2, 10 ,2): #range(start , stop , step)
+    
+#     print(i)
+
+for i in range (2 , 101, 2):
+    print(i)
