90dc439da4e39907323de29d6623b0c6911f3b56
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-23 00:30:48 +0530

    loop10.py

--- variable/loop/loop9.py
+++ variable/loop/loop9.py
@@ -10,7 +10,68 @@
 # print the multiplication table of a number n
 
 
-n = int(input("enter the number :-"))
+# n = int(input("enter the number :-"))
 
-for i in range(1, 11):
-    print(n*i)
+# for i in range(1, 11):
+#     print(n*i)
+
+
+
+# dusra question hi
+
+# for i in range(5):
+#     pass
+# print("some useful work")
+
+
+# wap to find the sum of first n number.(using while)
+# n = 5
+# for i in range(n+1):
+#     print(i)
+
+
+# n = 7
+
+# sum = 0
+
+# i = 1
+# while i <= n:
+#     sum +=1
+#     i +=1
+# # for i in range(1 , n+1):
+# #     sum += i
+
+#     print("total sum =",sum)
+
+# n = 7
+# sum = 0
+
+# i = 1
+# while i <= n:
+#     sum +=1
+#     i +=1
+
+#     print("total sum =",sum)
+
+# n = 7
+# total_sum = 0
+# i = 1
+
+# while i <= n:
+#     total_sum += i
+#     i += 1
+
+# print("Total sum =", total_sum)
+
+
+# wap to find the factorial of first n number (using for)
+
+n = 5
+fact = 1
+i = 1
+while i <=n:
+    fact *=i
+    i += 1
+
+    print("factorial :-", fact)
+
