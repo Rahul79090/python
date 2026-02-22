b6b4847525607193d3edef08185c43b3a2380fdc
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-22 23:12:00 +0530

    loop9.py

---
+++ variable/loop/loop9.py
@@ -0,0 +1,16 @@
+#print number from 1 to 100
+# for i in range(1 , 100):
+#     print(i)
+
+#print number from 100 to 1.
+
+# for i in range(100, 0 , -1):
+#     print(i)
+
+# print the multiplication table of a number n
+
+
+n = int(input("enter the number :-"))
+
+for i in range(1, 11):
+    print(n*i)
