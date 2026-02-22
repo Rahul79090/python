22caec33f8242f77b832fa3662469c0e4a120977
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-23 00:31:44 +0530

    loop10.py

--- variable/loop/loop10.py
+++ variable/loop/loop10.py
@@ -1,10 +1,22 @@
-# wap to find the factorial of first n number (using for)
+# # wap to find the factorial of first n number (using for)
 
-n = 5
-fact = 1
+# n = 5
+# fact = 1
+# i = 1
+# while i <=n:
+#     fact *=i
+#     i += 1
+
+#     print("factorial :-", fact)
+
+
+
+n = 7
+total_sum = 0
 i = 1
-while i <=n:
-    fact *=i
+
+while i <= n:
+    total_sum += i
     i += 1
 
-    print("factorial :-", fact)
+print("Total sum =", total_sum)
--- variable/loop/tempCodeRunnerFile.py
+++ variable/loop/tempCodeRunnerFile.py
@@ -1,7 +1,10 @@
-i = 0
-# while i <= 5:
-#     if (i ==3):
-#         i += 1
-#         continue
-#     print(i)
-#     i +=1
+
+n = 7
+total_sum = 0
+i = 1
+
+while i <= n:
+    total_sum += i
+    i += 1
+
+print("Total sum =", total_sum)
