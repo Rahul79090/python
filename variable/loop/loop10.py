cda46918f2068e76f398c4c8f13c5f72bfb8f736
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-24 21:06:42 +0530

    loop10.py

--- variable/loop/loop10.py
+++ variable/loop/loop10.py
@@ -11,12 +11,22 @@
 
 
 
-n = 7
-total_sum = 0
-i = 1
+# n = 7
+# total_sum = 0
+# i = 1
 
-while i <= n:
-    total_sum += i
-    i += 1
+# while i <= n:
+#     total_sum += i
+#     i += 1
 
-print("Total sum =", total_sum)
+# print("Total sum =", total_sum)
+
+
+ # wap to find the factorial of first n number (using for)
+
+n = 5
+fact = 1
+for i in range(1, n+1):
+    fact *=i
+
+    print("factrial =", fact)
