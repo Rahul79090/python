a53bd29112bd3ed9c8ae099dad4ee7a9a6501e08
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-26 23:17:34 +0530

    loop12.py

--- variable/loop/loop12.py
+++ variable/loop/loop12.py
@@ -11,7 +11,12 @@
 # else:
 #     print("not found")
 
+# n = 5
+
+# for i in range(1, n + 1):
+#     print("*" * i)
+
 n = 5
 
-for i in range(1, n + 1):
+for i in range(n, 0, -1):
     print("*" * i)
