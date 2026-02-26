1d00d9835ea9fd480793f7566ede9103063a10fd
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-26 22:59:58 +0530

    loop12.py

--- variable/loop/loop12.py
+++ variable/loop/loop12.py
@@ -1,12 +1,17 @@
-list = (1 , 4, 9, 16, 25, 49, 64, 81, 100,49)
+# list = (1 , 4, 9, 16, 25, 49, 64, 81, 100,49)
 
-x =49
-ind = 0
-for num in list:
-    if(num == x):
-        print("element is found ",ind)
-        ind +=1
-        break
-    print(num)
-else:
-    print("not found")
+# x =49
+# ind = 0
+# for num in list:
+#     if(num == x):
+#         print("element is found ",ind)
+#         ind +=1
+#         break
+#     print(num)
+# else:
+#     print("not found")
+
+n = 5
+
+for i in range(1, n + 1):
+    print("*" * i)
