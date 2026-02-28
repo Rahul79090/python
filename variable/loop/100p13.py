0ca561d34fbd7d58af1aaff99d6d99fcf4fe4fd4
Rahul79090 <kumarrahul032004@gmail.com>
2026-02-28 23:36:17 +0530

    ioop13.py

--- variable/loop/100p13.py
+++ variable/loop/100p13.py
@@ -1,8 +1,21 @@
-n = 4
-num = 1
+# n = 4
+# num = 1
 
-for i in range(1, n + 1):
-    for j in range(i):
-        print(num, end=" ")
-        num += 1
-    print()
+# for i in range(1, n + 1):
+#     for j in range(i):
+#         print(num, end=" ")
+#         num += 1
+#     print()
+
+
+# print number from 1 to 50 using a loop
+
+# for i in range(1 , 101):
+#     print(i)
+
+# print all even number between 1 and 100
+for i in range(1,100):
+    if(i/2==0):
+        print(i)
+        # else:
+            # print
--- variable/loop/loop12.py
+++ variable/loop/loop12.py
@@ -16,7 +16,42 @@
 # for i in range(1, n + 1):
 #     print("*" * i)
 
-n = 5
+# n = 5
 
-for i in range(n, 0, -1):
-    print("*" * i)
+# for i in range(n, 0, -1):
+#     print("*" * i)
+
+
+# even number ke liye 
+# i = 1
+# while i<=100:
+#     if(i%2 !=0):
+#         i +=1
+#         continue #skip
+#     print(i)
+#     i +=1
+
+# i = 1
+# for i in range(1, 100):
+#     if(i%2 !=0):
+#         i+=1
+#         continue
+#     print(i)
+#     i +=1
+
+# Print all odd numbers between 1 and 100
+# i = 0
+# for i in range(1, 100):
+#     if(i%2 !=1):
+#         i+=1
+#         continue
+#     print(i)
+#     i +=1
+
+# find the sum of first n natural number
+# n = int(input("enter the natural number"))
+# sum = n*(n+1)/2
+# print("sum is :-", sum)
+
+
+# Find the factorial of a given number.
