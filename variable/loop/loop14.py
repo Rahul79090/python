f0c03641afae109791efac655b53520799fd7966
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-01 23:55:53 +0530

    loop14.py

--- variable/loop/loop14.py
+++ variable/loop/loop14.py
@@ -5,6 +5,15 @@
 
 
 # Find the factorial of a given number.
-n = int(input("enter the natural number"))
-sum = n*(n+1)/2
-print("sum is :-", sum)
+# n = int(input("enter the natural number"))
+# sum = n*(n+1)/2
+# print("sum is :-", sum)
+
+
+i = 1
+while i <=10:
+    if(i ==3):
+        i +=1
+        continue
+    print(i)
+    i +=1
