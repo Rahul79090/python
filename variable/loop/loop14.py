ccbe7115b8e4715af43e5f648051eee506c08893
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-01 23:55:25 +0530

    loop14.py

--- variable/loop/loop14.py
+++ variable/loop/loop14.py
@@ -1,7 +1,10 @@
 # find the sum of first n natural number
-n = int(input("enter the natural number"))
-sum = n*(n+1)/2
-print("sum is :-", sum)
+# n = int(input("enter the natural number"))
+# sum = n*(n+1)/2
+# print("sum is :-", sum)
 
 
 # Find the factorial of a given number.
+n = int(input("enter the natural number"))
+sum = n*(n+1)/2
+print("sum is :-", sum)
