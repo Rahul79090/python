b83048ad9680ba7b69139eba0ac012c183876919
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-02 23:47:21 +0530

    fn1.py

---
+++ variable/loop/function/fn1.py
@@ -0,0 +1,29 @@
+def calc_sum(a , b):
+    sum = a+b
+    print(sum)
+    return sum
+
+
+    calc_sum(5 , 10)
+
+# a = 2
+# b = 3
+
+# sum = a + b
+# print(sum)
+
+#more line of code 
+
+calc_sum(2 , 10)
+# a = 2
+# b = 10
+# sum = a+b
+# print(sum)
+
+#more line of code 
+
+calc_sum(12, 17)
+# a = 12 
+# b =17
+# sum = a+b
+# print(sum)
