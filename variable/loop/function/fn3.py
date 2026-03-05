3d9cac933b0a1f6c3dc463608f785c94fcd9a7a4
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-05 23:49:17 +0530

    fn3.py

--- variable/loop/function/fn3.py
+++ variable/loop/function/fn3.py
@@ -8,12 +8,19 @@
 
 # calc_sum(20, 14)
 
-def calc_sum(a , b):
-    sum = a+b
-    print(sum)
-    return sum
+# def calc_sum(a , b):
+#     sum = a+b
+#     print(sum)
+#     return sum
 
 
-calc_sum(16, 10)
+# calc_sum(16, 10)
 
-calc_sum(20, 14)
+# calc_sum(20, 14)
+i = 0
+while i <= 5:
+    if (i ==3):
+        i += 1
+        continue
+    print(i)
+    i +=6
