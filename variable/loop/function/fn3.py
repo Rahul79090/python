f19723b16ec567f6c7fa9d5b052984297ebcdcd2
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-06 23:37:00 +0530

    fn3.py

--- variable/loop/function/fn3.py
+++ variable/loop/function/fn3.py
@@ -17,10 +17,18 @@
 # calc_sum(16, 10)
 
 # calc_sum(20, 14)
-i = 0
-while i <= 5:
-    if (i ==3):
-        i += 1
-        continue
-    print(i)
-    i +=6
+# i = 0
+# while i <= 5:
+#     if (i ==3):
+#         i += 1
+#         continue
+#     print(i)
+#     i +=6
+
+# find the sum of first n natural number
+n = int(input("enter the natural number"))
+sum = n*(n+1)/2
+print("sum is :-", sum)
+
+
+# Find the factorial of a given number.
