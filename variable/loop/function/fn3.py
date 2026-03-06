6b720b98d445cf343ae523a7cbc1d1a2844b62d6
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-06 23:39:07 +0530

    loop12.py

--- variable/loop/function/fn3.py
+++ variable/loop/function/fn3.py
@@ -26,9 +26,9 @@
 #     i +=6
 
 # find the sum of first n natural number
-n = int(input("enter the natural number"))
-sum = n*(n+1)/2
-print("sum is :-", sum)
+# n = int(input("enter the natural number"))
+# sum = n*(n+1)/2
+# print("sum is :-", sum)
 
 
 # Find the factorial of a given number.
