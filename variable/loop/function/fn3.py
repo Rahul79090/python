07354aaffa27319c1a78da20a61a671df204eac4
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-07 22:23:03 +0530

    fun4.py

--- variable/loop/function/fn3.py
+++ variable/loop/function/fn3.py
@@ -32,3 +32,7 @@
 
 
 # Find the factorial of a given number.
+
+n = int(input("enter the natural number"))
+sum = n*(n+1)/2
+print("sum is :-", sum)
---
+++ variable/loop/function/fun4.py
@@ -0,0 +1,3 @@
+n = int(input("enter the natural number"))
+sum = n*(n+1)/2
+print("sum is :-", sum)
