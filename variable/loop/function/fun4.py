4f46877475feee5e8b0d7b093107f3e7ccd1411a
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-09 00:00:26 +0530

    fun4.py

--- variable/loop/function/fun4.py
+++ variable/loop/function/fun4.py
@@ -1,3 +1,9 @@
-n = int(input("enter the natural number"))
-sum = n*(n+1)/2
-print("sum is :-", sum)
+# n = int(input("enter the natural number"))
+# sum = n*(n+1)/2
+# print("sum is :-", sum)
+
+
+# n = int(input("enter the natural number"))
+# sum = n*(n+1)/2
+# print("sum is :-", sum)
+
