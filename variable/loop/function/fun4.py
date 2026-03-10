4f33b414495b9d90c64ab0eb790ec51ef68dc2dc
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-10 21:18:49 +0530

    fun5.py

--- variable/loop/function/fun4.py
+++ variable/loop/function/fun4.py
@@ -1,6 +1,13 @@
+# def calc_sum(a , b):
+#     return a+b
+
+# sum = calc_sum(12, 2)
+# print(sum)    
+
 def calc_sum(a , b):
     return a+b
 
 sum = calc_sum(12, 2)
 print(sum)    
+
 
---
+++ variable/loop/function/fun5.py
@@ -0,0 +1,5 @@
+def calc_sum(a , b):
+    return a+b
+
+sum = calc_sum(12, 2)
+print(sum)    
