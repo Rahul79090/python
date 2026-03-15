72210490406eafe3bb79548fd66bc60239e19f11
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-15 22:41:06 +0530

    fn1.py

--- variable/operator/function /fn1.py
+++ variable/operator/function /fn1.py
@@ -5,6 +5,15 @@
 # cal_prod()
 
 
+# cities = ["delhi", "gurgaon","noida","pune","chennai"]
+# heroes = ["thor", "ironman","captain","america","shaktiman"]
+
+# def print_len(list):
+#     print(len(list))
+
+# print_len(cities)
+# print_len(heroes)
+
 cities = ["delhi", "gurgaon","noida","pune","chennai"]
 heroes = ["thor", "ironman","captain","america","shaktiman"]
 
