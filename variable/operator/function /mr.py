24b9fe42bacdb24cb476055d7a474afc10b9505b
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-16 23:10:14 +0530

    mr.py

--- variable/operator/function /mr.py
+++ variable/operator/function /mr.py
@@ -5,10 +5,18 @@
 # print(a)
 # print(a[0])
 
-def even_odd(num):
-    if num % 2 == 0:
-        print("Even number")
-    else:
-        print("Odd number")
+# def even_odd(num):
+#     if num % 2 == 0:
+#         print("Even number")
+#     else:
+#         print("Odd number")
 
-even_odd(7)
+# even_odd(7)
+
+# import array as arr
+
+a = arr.array('i', [1, 2, 3, 4, 5])
+
+print(a)
+print(a[0])
+
