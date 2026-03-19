d86cd516409e09aaf7f907b2a06bccad7cca3219
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-19 23:02:57 +0530

    re.py

--- variable/recursion/re.py
+++ variable/recursion/re.py
@@ -19,6 +19,16 @@
 
 # write a recursive function to calculate the sum of first n natural numbers.
 
+# n = int(input("enter the value of n:-"))
+# def calc_sum(n):
+#     if(n ==0):
+#         return 0
+#     # print(n)
+#     return calc_sum(n-1)+n
+
+# sum = calc_sum(n)
+# print(sum)
+
 n = int(input("enter the value of n:-"))
 def calc_sum(n):
     if(n ==0):
