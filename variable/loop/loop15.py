7ea215e4dab7a3851874c28bd19dd800ed458389
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-22 21:20:04 +0530

    loop15.py

--- variable/loop/loop15.py
+++ variable/loop/loop15.py
@@ -1,10 +1,15 @@
-# i = 1
-# while i <=10:
-#     if(i ==3):
-#         i +=1
-#         continue
-#     print(i)
-#     i +=1
+# # i = 1
+# # while i <=10:
+# #     if(i ==3):
+# #         i +=1
+# #         continue
+# #     print(i)
+# #     i +=1
 
-for i in range(10 , 0 , -1):
+# for i in range(10 , 0 , -1):
+#     print("*" *i)
+
+
+    
+for i in range(10 , 0 , -2):
     print("*" *i)
