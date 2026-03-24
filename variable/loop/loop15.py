3450ec010dce0fa3c7bac5ea669bcbfe5ed142ca
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-24 23:14:38 +0530

    loop15.py

--- variable/loop/loop15.py
+++ variable/loop/loop15.py
@@ -14,5 +14,13 @@
 # for i in range(10 , 0 , -2):
 #     print("*" *i)
 
-for i in range(4 , 0, -1):
-    print("*" *i)
+# for i in range(4 , 0, -1):
+#     print("*" *i)
+
+n = int(input("Enter a number: "))
+fact = 1
+
+for i in range(1, n + 1):
+    fact *= i
+
+print("Factorial =", fact)
