93ae8e3ab11c2152e68f01b2ce8b5cede3ecf794
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-25 22:51:38 +0530

    loop14.py

--- variable/loop/loop14.py
+++ variable/loop/loop14.py
@@ -10,10 +10,18 @@
 # print("sum is :-", sum)
 
 
-i = 1
-while i <=10:
-    if(i ==3):
-        i +=1
-        continue
-    print(i)
-    i +=1
+# i = 1
+# while i <=10:
+#     if(i ==3):
+#         i +=1
+#         continue
+#     print(i)
+#     i +=1
+
+n = int(input("Enter a number: "))
+fact = 1
+
+for i in range(1, n + 1):
+    fact *= i
+
+print("Factorial =", fact)
