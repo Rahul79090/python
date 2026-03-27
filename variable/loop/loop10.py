c39375d534a1054419ac2ba82846d37e2039506a
Rahul79090 <kumarrahul032004@gmail.com>
2026-03-27 23:02:25 +0530

    loop10.py

--- variable/loop/loop10.py
+++ variable/loop/loop10.py
@@ -24,6 +24,13 @@
 
  # wap to find the factorial of first n number (using for)
 
+# n = 5
+# fact = 1
+# for i in range(1, n+1):
+#     fact *=i
+
+#     print("factrial =", fact)
+
 n = 5
 fact = 1
 for i in range(1, n+1):
