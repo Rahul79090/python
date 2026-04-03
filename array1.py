ddcad5ab4863ee691fb6eb5d83912894893202ba
Rahul79090 <kumarrahul032004@gmail.com>
2026-04-03 23:06:40 +0530

    array1.py

--- array1.py
+++ array1.py
@@ -5,9 +5,16 @@
 
 # print(arr[-2:])
 
-arr = [10, 20, 30, 40, 50]
+# arr = [10, 20, 30, 40, 50]
 
-# for i in arr:
-print(arr[ -3:])
+# # for i in arr:
+# print(arr[ -3:])
 
-print(arr[-2:])
+# print(arr[-2:])
+
+marks = int(input("Enter marks: "))
+
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
