decbfa6d48e3d300c685d4363d55a17972e0f8b0
Rahul79090 <kumarrahul032004@gmail.com>
2026-04-05 20:49:10 +0530

    s.py

--- s.py
+++ s.py
@@ -2,6 +2,9 @@
 # print (even_value)
 # name = input ("enter your name here :-")
 # print(name)
+
+
+
 age = float (input("enter the age:-"))
 print(age)
 exp1 = eval(input("enter any equation here:-"))
