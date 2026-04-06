daff0f80fea8d6670234982097bd11a05a7936a0
Rahul79090 <kumarrahul032004@gmail.com>
2026-04-06 23:31:22 +0530

    d.py

--- d.py
+++ d.py
@@ -1,8 +1,14 @@
-a = 5
-b = 2
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
-print(a % b)
-print(a ** b) # ** matlab power hota hi kisi chije ka ** ka matlab
+# a = 5
+# b = 2
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a ** b) # ** matlab power hota hi kisi chije ka ** ka matlab
+
+
+age = float (input("enter the age:-"))
+print(age)
+exp1 = eval(input("enter any equation here:-"))
+print(exp1)
--- s.py
+++ s.py
@@ -2,10 +2,9 @@
 # print (even_value)
 # name = input ("enter your name here :-")
 # print(name)
-
-
-
 age = float (input("enter the age:-"))
 print(age)
 exp1 = eval(input("enter any equation here:-"))
 print(exp1)
+
+
