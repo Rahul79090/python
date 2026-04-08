c3e3ed407e17cdeb56fa2c2521e5e3ffc421fe7c
Rahul79090 <kumarrahul032004@gmail.com>
2026-04-08 22:45:48 +0530

    c.py

--- c.py
+++ c.py
@@ -1,6 +1,12 @@
-marks = int(input("Enter marks: "))
+# marks = int(input("Enter marks: "))
 
-if marks >= 40:
-    print("Pass")
-else:
-    print("Fail")
+# if marks >= 40:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+age = float (input("enter the age:-"))
+print(age)
+exp1 = eval(input("enter any equation here:-"))
+print(exp1)
+
