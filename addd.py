2f6b4c82185f8b15fd756a2657c45f5ad0b984b8
Rahul79090 <kumarrahul032004@gmail.com>
2026-04-13 22:49:48 +0530

    addd.py

--- addd.py
+++ addd.py
@@ -3,7 +3,12 @@
 # sum = a + b
 # print("Sum =", sum)
 
-a = 10
-b = 20
-sum = a + b
-print("Sum =", sum)
+# a = 10
+# b = 20
+# sum = a + b
+# print("Sum =", sum)
+
+
+arr = [10, 20, 30, 40, 50]
+print(arr)
+
