ee4385a8bc9767ab28230f64c87162be53fdc222
Rahul79090 <kumarrahul032004@gmail.com>
2026-04-17 23:24:58 +0530

    loop16.py

--- variable/loop/loop16.py
+++ variable/loop/loop16.py
@@ -1,3 +1,22 @@
 
-for i in range(1 , 5) :
-    print(i)
+# for i in range(1 , 5) :
+#     print(i)
+
+name ="rahul kuamr"
+age = 19
+price = 25.6
+print("my name is :-", name)
+print("my age is :-",age)
+print("my price is :-",price )
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+name1 = "rkg"
+name2 = 'rkg'
+name3 = '''rkg'''
+
+print(name1)
+print(name2)
+print(name3)
