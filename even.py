3586e7d0449a63c92a166bfded111851860fd399
Rahul79090 <kumarrahul032004@gmail.com>
2026-04-19 23:18:38 +0530

    even.py

--- even.py
+++ even.py
@@ -1,6 +1,15 @@
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-if num % 2 == 0:
-    print("Even number")
-else:
-    print("Odd number")
+# if num % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+
+
+count =100
+while count >= 1:
+    print(count)
+    count-=1
+
+ print the multi[lication table of a number n.
