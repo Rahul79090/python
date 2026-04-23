8ca3cd7a3dc5559036f8d2c4aed72e8cfe4c9610
Rahul79090 <kumarrahul032004@gmail.com>
2026-04-23 23:28:42 +0530

    arr23.py

--- arr23.py
+++ arr23.py
@@ -1,9 +1,18 @@
-arr = [5, 2, 9, 1, 7]
+# arr = [5, 2, 9, 1, 7]
 
-max_val = arr[0]
+# max_val = arr[0]
 
-for num in arr:
-    if num > max_val:
-        max_val = num
+# for num in arr:
+#     if num > max_val:
+#         max_val = num
 
-print("Maximum =", max_val)
+# print("Maximum =", max_val)
+
+n = int(input("Enter number of elements: "))
+arr = []
+
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+
+print("Array is:", arr)
