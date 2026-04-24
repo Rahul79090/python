9b1ab50dc3d32e71e24c85625636cf10594e4036
Rahul79090 <kumarrahul032004@gmail.com>
2026-04-24 23:33:48 +0530

    add.py

--- add.py
+++ add.py
@@ -1,9 +1,18 @@
-a = 123
-b = 1.23
+# a = 123
+# b = 1.23
 
-print(type(a))
-print(type(b))
+# print(type(a))
+# print(type(b))
 
-c = a + b
-print(c)
-print(type(c))
+# c = a + b
+# print(c)
+# print(type(c))
+
+ = int(input("Enter number of elements: "))
+arr = []
+
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+
+print("Array is:", arr)
