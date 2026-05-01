b04e4bc336212266b9878236f0d97aaba1c7ef22
Rahul79090 <kumarrahul032004@gmail.com>
2026-05-01 23:38:10 +0530

    fdd.py

--- fdd.py
+++ fdd.py
@@ -1,5 +1,13 @@
 
-h=int (input())
-while (h>10):
-    print("rahul")
-    h=h-1
+# h=int (input())
+# while (h>10):
+#     print("rahul")
+#     h=h-1
+
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
