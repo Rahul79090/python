76d33efa4bb0ae5b5977ab354b8c29be77240e20
Rahul79090 <kumarrahul032004@gmail.com>
2026-05-07 23:22:12 +0530

    loop20.py

--- variable/loop/loop20.py
+++ variable/loop/loop20.py
@@ -1,8 +1,15 @@
 
-i = 0
-while i <= 5:
-    if (i ==3):
-        i += 1
-        continue
-    print(i)
-    i +=1
+# i = 0
+# while i <= 5:
+#     if (i ==3):
+#         i += 1
+#         continue
+#     print(i)
+#     i +=1
+
+# Function definition
+def greet():
+    print("Hello, Welcome to Python!")
+
+# Function call
+greet()
