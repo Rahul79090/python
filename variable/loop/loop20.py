3b4431ff66e10a6ee7dc4e365645b84765dd7a42
Rahul79090 <kumarrahul032004@gmail.com>
2026-05-12 23:41:22 +0530

    loop20.py

--- variable/loop/loop20.py
+++ variable/loop/loop20.py
@@ -15,9 +15,20 @@
 # greet()
 
 
-# Function definition
-def greet():
-    print("Hello, Welcome to Python!")
+# # Function definition
+# def greet():
+#     print("Hello, Welcome to Python!")
 
-# Function call
-greet()
+# # Function call
+# greet()
+
+
+arr = [5, 2, 9, 1, 7]
+
+max_val = arr[0]
+
+for num in arr:
+    if num > max_val:
+        max_val = num
+
+print("Maximum =", max_val)
