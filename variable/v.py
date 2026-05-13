14a3980638891a1e8a435424c698f625eca6151d
Rahul79090 <kumarrahul032004@gmail.com>
2026-05-13 23:56:37 +0530

    v.py

--- variable/v.py
+++ variable/v.py
@@ -1,25 +1,37 @@
-name ="rahul kuamr"
-age = 19
-price = 25.6
-print("my name is :-", name)
-print("my age is :-",age)
-print("my price is :-",price )
+# name ="rahul kuamr"
+# age = 19
+# price = 25.6
+# print("my name is :-", name)
+# print("my age is :-",age)
+# print("my price is :-",price )
 
-print(type(name))
-print(type(age))
-print(type(price))
+# print(type(name))
+# print(type(age))
+# print(type(price))
 
-name1 = "rkg"
-name2 = 'rkg'
-name3 = '''rkg'''
+# name1 = "rkg"
+# name2 = 'rkg'
+# name3 = '''rkg'''
 
-print(name1)
-print(name2)
-print(name3)
+# print(name1)
+# print(name2)
+# print(name3)
 
 
-age =23
-old = False
-a = None
-print(type(old))
-print(type(a))
+# age =23
+# old = False
+# a = None
+# print(type(old))
+# print(type(a))
+
+
+
+arr = [5, 2, 9, 1, 7]
+
+max_val = arr[0]
+
+for num in arr:
+    if num > max_val:
+        max_val = num
+
+print("Maximum =", max_val)
