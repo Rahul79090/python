fc0cefbc422ad9348b234422df2e48c0fc7a1b39
Rahul79090 <kumarrahul032004@gmail.com>
2026-06-16 15:47:56 +0530

    l.py

---
+++ variable/loop/new/l.py
@@ -0,0 +1,20 @@
+# Write a Python program to perform addition, subtraction, multiplication, and division using arithmetic operators.
+# a = 2
+# b = 30
+# sub= b-a
+# add = a +b
+# print(add)
+
+# print(sub)
+
+# print(b/a)
+
+
+# Write a Python program to check whether a number is even or odd using operators.\
+
+a = int(input("Enter the value of a:- "))
+
+if a % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
